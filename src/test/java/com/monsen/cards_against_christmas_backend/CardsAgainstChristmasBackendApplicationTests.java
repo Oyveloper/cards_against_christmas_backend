3823f1e9b1f99d932d09df594605567f6329a3a5
Øyvind Monsen <yvind.monsen@gmail.com>
2020-12-17 15:55:12 +0100

    Project setup

---
+++ src/test/java/com/monsen/cards_against_christmas_backend/CardsAgainstChristmasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monsen.cards_against_christmas_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardsAgainstChristmasBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
