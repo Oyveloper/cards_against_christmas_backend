7413632b5ae6351dcb730708a9beedf74d204bc9
Øyvind Monsen <yvind.monsen@gmail.com>
2021-01-10 00:39:50 +0100

    Fixing the basic join websocket endpoint

--- src/main/java/com/monsen/cards_against_christmas_backend/web/controller/GamePlayController.java
+++ src/main/java/com/monsen/cards_against_christmas_backend/web/controller/GamePlayController.java
@@ -5,6 +5,7 @@ import com.monsen.cards_against_christmas_backend.web.DTO.GameUpdateDTO;
 import com.monsen.cards_against_christmas_backend.web.DTO.Message;
 import com.monsen.cards_against_christmas_backend.web.service.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
@@ -20,9 +21,9 @@ public class GamePlayController {
         this.gameService = gameService;
     }
 
-    @MessageMapping("/joinGame")
+    @MessageMapping("/joinGame/{gameId}")
     @SendTo("/topic/GameUpdate/{gameId}")
-    public Message<GameUpdateDTO> joinGame(@RequestParam("gameId") String gameId, @RequestParam("playerName") String playerName) {
+    public Message<GameUpdateDTO> joinGame(@DestinationVariable("gameId") String gameId, @RequestParam("playerName") String playerName) {
         Message<GameUpdateDTO> message = new Message<>();
         if (this.gameService.gameExists(gameId)) {
             CACGame game = gameService.getGame(gameId);
@@ -34,6 +35,8 @@ public class GamePlayController {
             message.setStatus("Error");
             message.setExplanation("Something went wrong i guess");
         }
+
+        System.out.println(message);
 
         return message;
     }
