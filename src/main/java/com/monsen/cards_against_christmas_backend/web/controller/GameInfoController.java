1b724f7a2b997c0e1014e546c9f34c58ab66d18c
Øyvind Monsen <yvind.monsen@gmail.com>
2021-01-11 19:00:12 +0100

    Standardizing the gameId parameter in all endpoints

--- src/main/java/com/monsen/cards_against_christmas_backend/web/controller/GameInfoController.java
+++ src/main/java/com/monsen/cards_against_christmas_backend/web/controller/GameInfoController.java
@@ -1,5 +1,6 @@
 package com.monsen.cards_against_christmas_backend.web.controller;
 
+import com.monsen.cards_against_christmas_backend.game.Player;
 import com.monsen.cards_against_christmas_backend.web.DTO.Message;
 import com.monsen.cards_against_christmas_backend.web.service.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,18 +25,32 @@ public class GameInfoController {
 
 
     @GetMapping("/gameExists")
-    public ResponseEntity<Message<Boolean>> gameExists(@RequestParam String id) {
-        return new ResponseEntity<>(new Message<>(this.gameService.gameExists(id), "OK", ""), HttpStatus.OK);
+    public ResponseEntity<Message<Boolean>> gameExists(@RequestParam String gameId) {
+        return new ResponseEntity<>(new Message<>(this.gameService.gameExists(gameId), "OK", ""), HttpStatus.OK);
     }
 
     @PostMapping("/createGame")
-    public ResponseEntity<Message<?>> createGame(@RequestParam String id) {
-        if (this.gameService.gameExists(id)) {
+    public ResponseEntity<Message<?>> createGame(@RequestParam String gameId) {
+        if (this.gameService.gameExists(gameId)) {
             return new ResponseEntity<>(HttpStatus.CONFLICT);
         }
-        this.gameService.createGame(id);
+        this.gameService.createGame(gameId);
 
         return new ResponseEntity<>(new Message<>(null, "OK", ""), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/isPlayerNameTaken")
+    public ResponseEntity<Message<Boolean>> isPlayerNameTaken(@RequestParam("playerName") String playerName, @RequestParam("gameId") String gameId) {
+        if (!this.gameService.gameExists(gameId)) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        boolean nameTaken = this.gameService.getGame(gameId)
+                .getPlayers()
+                .stream()
+                .map(Player::getName)
+                .anyMatch(n -> n.equals(playerName));
+
+        return new ResponseEntity<>(new Message<>(nameTaken, "OK", ""), HttpStatus.OK);
     }
 
 }
